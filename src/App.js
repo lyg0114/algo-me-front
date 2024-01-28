6a6839c39b2bdbd7e94fbd0d9ac0abdc6b34a37b
lyg0114 <whdnseowkd@naver.com>
2024-01-28 13:58:22 +0100

    feat : add tailwind css sample

--- src/App.js
+++ src/App.js
@@ -1,28 +1,79 @@
 import React from 'react'
-import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
-import {AuthProvider} from './components/context/AuthContext'
-import PrivateRoute from './components/misc/PrivateRoute'
-import Home from './components/home/Home'
-import Login from './components/home/Login'
-import Signup from './components/home/Signup'
-import AdminPage from './components/admin/AdminPage'
-import UserPage from './components/user/UserPage'
+import Test from "./components/home/Test";
+
+const products = [
+    {
+        _id: "100001",
+        img: "https://images.pexels.com/photos/258244/pexels-photo-258244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        productName: "Round Table Clock",
+        price: "44.00",
+        color: "Black",
+    },
+    {
+        _id: "100002",
+        img: "https://images.pexels.com/photos/258244/pexels-photo-258244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        productName: "Round Table Clock",
+        price: "44.00",
+        color: "Black",
+    },
+    {
+        _id: "100003",
+        img: "https://images.pexels.com/photos/258244/pexels-photo-258244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        productName: "Round Table Clock",
+        price: "44.00",
+        color: "Black",
+    },
+    {
+        _id: "100004",
+        img: "https://images.pexels.com/photos/258244/pexels-photo-258244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        productName: "Round Table Clock",
+        price: "44.00",
+        color: "Black",
+    },
+    {
+        _id: "100005",
+        img: "https://images.pexels.com/photos/258244/pexels-photo-258244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        productName: "Round Table Clock",
+        price: "44.00",
+        color: "Black",
+    },
+];
+
 
 function App() {
     return (
-        <AuthProvider>
-            <Router>
-                <Routes>
-                    <Route path='/' element={<Home/>}/>
-                    <Route path='/login' element={<Login/>}/>
-                    <Route path='/signup' element={<Signup/>}/>
-                    <Route path="/adminpage" element={<PrivateRoute><AdminPage/></PrivateRoute>}/>
-                    <Route path="/userpage" element={<PrivateRoute><UserPage/></PrivateRoute>}/>
-                    <Route path="*" element={<Navigate to="/"/>}/>
-                </Routes>
-            </Router>
-        </AuthProvider>
-    )
+        <div className="container mx-auto my-96">
+            <div className="grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-4 gap-4">
+                {products.map((product, index) => (
+                    <Test
+                        key={index}
+                        _id={product._id}
+                        img={product.img}
+                        productName={product.productName}
+                        price={product.price}
+                        color={product.color}
+                    />
+                ))}
+            </div>
+        </div>)
 }
+
+// function App() {
+//     return (
+//         <AuthProvider>
+//             <Router>
+//                 <Routes>
+//                     <Route path='/' element={<Home/>}/>
+//                     <Route path='/test' element={<Home/>}/>
+//                     <Route path='/login' element={<Login/>}/>
+//                     <Route path='/signup' element={<Signup/>}/>
+//                     <Route path="/adminpage" element={<PrivateRoute><AdminPage/></PrivateRoute>}/>
+//                     <Route path="/userpage" element={<PrivateRoute><UserPage/></PrivateRoute>}/>
+//                     <Route path="*" element={<Navigate to="/"/>}/>
+//                 </Routes>
+//             </Router>
+//         </AuthProvider>
+//     )
+// }
 
 export default App
