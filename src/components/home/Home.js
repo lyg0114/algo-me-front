3daa162f703737a5b2993017d952709de54ab21c
lyg0114 <whdnseowkd@naver.com>
2024-02-03 14:34:32 +0100

    refactor : delete auth code

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,24 +6,13 @@ import HomeDashBoard from "./dashboard/HomeDashBoard";
 function Home() {
     const Auth = useAuth()
     const isLoggedIn = Auth.userIsAuthenticated()
-    const [isLoading, setIsLoading] = useState(false)
-
-    if (isLoading) {
-        return (
-            <div>### LOADING ###</div>
-        )
-    }
 
     if (isLoggedIn) {
         return (<HomeDashBoard/>)
     }
 
     return (
-        <div>
-            Logout HOME
-            <div>Don't have already an account?</div>
-            <NavLink to="/signup" color='violet' as={NavLink}>Sign Up</NavLink>
-        </div>
+        <NavLink to="/signup" color='violet' as={NavLink}/>
     )
 }
 
