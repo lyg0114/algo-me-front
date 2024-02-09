6ba9ae1fa902af9c7374099af22280c822dee8d9
lyg0114 <whdnseowkd@naver.com>
2024-02-09 15:48:15 +0100

    feat : add react-infinite-scroll-compone

--- src/components/user/home/dashboard/HomeDashBoard.js
+++ src/components/user/home/dashboard/HomeDashBoard.js
@@ -2,17 +2,18 @@ import {useAuth} from "../../../context/AuthContext";
 import {orderApi} from "../../../util/OrderApi";
 import {handleLogError} from "../../../util/Helpers";
 import React, {useEffect, useState} from "react";
-import QuestionCard from "../QuestionCard";
 import {NavLink, useNavigate} from "react-router-dom";
 import ViewDetailModal from "../modal/ViewDetailModal";
+import InfiniteScroll from "react-infinite-scroll-component";
+import QuestionCard from "../QuestionCard";
 
 function HomeDashBoard() {
     const Auth = useAuth();
     const [isError, setError] = useState(null);
-    const [questions, setQuestions] = useState(null);
     const [question, setQuestion] = useState(null);
     const [isViewDetailModalOpen, setIsViewDetailModalOpen] = useState(false);
     const navigate = useNavigate();
+    const [questions, setQuestions] = useState([]);
 
     useEffect(() => {
         fetchQuestions();
@@ -23,7 +24,9 @@ function HomeDashBoard() {
         try {
             const response = await orderApi.getQuestions(user);
             const result = await response.data.content;
-            setQuestions(result);
+            setTimeout(() => {
+                setQuestions(prevItems => prevItems.concat(result));
+            }, 1500);
         } catch (error) {
             handleLogError(error);
             setError(error);
@@ -61,9 +64,15 @@ function HomeDashBoard() {
                     <NavLink className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                              to="/save-question/" color='violet' as={NavLink}>문제등록</NavLink>
                 </div>
+
                 <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                    {questions !== null &&
-                        questions.map((question, index) => (
+                    <InfiniteScroll
+                        dataLength={questions.length}
+                        next={fetchQuestions}
+                        hasMore={true}
+                        loader={<h2>Loading...</h2>}
+                    >
+                        {questions.map((question, index) => (
                             <QuestionCard
                                 key={index}
                                 id={question.id}
@@ -74,6 +83,7 @@ function HomeDashBoard() {
                                 getQuestionDetail={getQuestionDetail}
                             />
                         ))}
+                    </InfiniteScroll>
                 </div>
             </div>
         </div>
