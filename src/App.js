346cbe7c5903ac08ad88e4c16d584b036cde08fb
lyg0114 <whdnseowkd@naver.com>
2024-02-28 14:07:45 +0100

    feat : 로그인창 적용

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@ import 'react-bootstrap/dist/react-bootstrap.min'
 import Login from "./components/Login";
 import QuestionsLayout from "./components/user/question/QuestionsLayout";
 import QuestionSaveLayout from "./components/user/question/QuestionSave/QuestionSaveLayout";
+import {AuthProvider} from "./components/context/AuthContext";
+import PrivateRoute from "./components/util/PrivateRoute";
 
 function App() {
     return (
         <>
-            <Router>
-                <Routes>
-                    <Route path='/' element={<QuestionsLayout/>}/>
-                    <Route path="/save-question/:id?" element={<QuestionSaveLayout/>}/>
-                    <Route path="*" element={<Navigate to="/"/>}/>
-                    <Route path='/login' element={<Login/>}/>
-                    <Route path="*" element={<Navigate to="/"/>}/>
-                </Routes>
-            </Router>
+            <AuthProvider>
+                <Router>
+                    <Routes>
+                        <Route path='/' element={
+                            <PrivateRoute>
+                                <QuestionsLayout/>
+                            </PrivateRoute>
+                        }/>
+                        <Route path="/save-question/:id?" element={<QuestionSaveLayout/>}/>
+                        <Route path="*" element={<Navigate to="/"/>}/>
+                        <Route path='/login' element={<Login/>}/>
+                        <Route path="*" element={<Navigate to="/"/>}/>
+                    </Routes>
+                </Router>
+            </AuthProvider>
         </>
     )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,97 +1,92 @@
-import React, {useState} from 'react'
-import {Navigate} from 'react-router-dom'
-import {useAuth} from './context/AuthContext'
-import {backendApi} from './util/BackendApi'
-import {handleLogError, parseJwt} from './util/Helpers'
+import React, {useState} from 'react';
+import {Navigate} from 'react-router-dom';
+import {useAuth} from './context/AuthContext';
+import {backendApi} from './util/BackendApi';
+import {handleLogError, parseJwt} from './util/Helpers';
+import {Button, Form} from 'react-bootstrap';
 
 function Login() {
-
-  const Auth = useAuth()
-  const isLoggedIn = Auth.userIsAuthenticated()
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [isError, setIsError] = useState(false)
+  const Auth = useAuth();
+  const isLoggedIn = Auth.userIsAuthenticated();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isError, setIsError] = useState(false);
 
   const handleInputChange = (e) => {
-    const { name, value } = e.target; // Destructure from e.target
+    const {name, value} = e.target;
     if (name === 'username') {
       setUsername(value);
     } else if (name === 'password') {
       setPassword(value);
     }
-  }
+  };
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (!(username && password)) {
-      setIsError(true)
-      return
+      setIsError(true);
+      return;
     }
 
     try {
-      const response = await backendApi.authenticate(username, password)
-      const  accessToken  = response.data.token;
-      const data = parseJwt(accessToken)
-      const authenticatedUser = { data, accessToken }
-      Auth.userLogin(authenticatedUser)
-      setUsername('')
-      setPassword('')
-      setIsError(false)
+      const response = await backendApi.authenticate(username, password);
+      const accessToken = response.data.token;
+      const data = parseJwt(accessToken);
+      const authenticatedUser = {data, accessToken};
+      Auth.userLogin(authenticatedUser);
+      setUsername('');
+      setPassword('');
+      setIsError(false);
     } catch (error) {
-      handleLogError(error)
-      setIsError(true)
+      handleLogError(error);
+      setIsError(true);
     }
-  }
+  };
 
   if (isLoggedIn) {
-    return <Navigate to={'/'} />
+    return <Navigate to={'/'}/>;
   }
 
   return (
       <div className="min-h-screen flex items-center justify-center bg-gray-900">
         <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
           <h2 className="text-3xl font-extrabold text-gray-900 mb-4">LOGIN</h2>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">
-               ID
-              </label>
-              <input
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
+              <Form.Label>ID</Form.Label>
+              <Form.Control
                   type="text"
-                  id="username"
-                  name="username"
                   value={username}
                   onChange={handleInputChange}
-                  className="w-full p-2 border rounded-md"
                   placeholder="Enter your id"
+                  name="username"
                   required
               />
-            </div>
-            <div className="mb-6">
-              <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
-               PW
-              </label>
-              <input
+            </Form.Group>
+
+            <Form.Group controlId="password">
+              <Form.Label>PW</Form.Label>
+              <Form.Control
                   type="password"
-                  id="password"
-                  name="password"
                   value={password}
                   onChange={handleInputChange}
-                  className="w-full p-2 border rounded-md"
                   placeholder="Enter your password"
+                  name="password"
                   required
               />
-            </div>
-            <button
+            </Form.Group>
+
+            <Button
+                variant="primary"
                 type="submit"
-                className="w-full bg-blue-500 text-white font-bold p-2 rounded-md hover:bg-blue-600 transition duration-300"
+                className="w-full hover:bg-blue-600 transition duration-300"
             >
               Login
-            </button>
-          </form>
+            </Button>
+          </Form>
         </div>
       </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
