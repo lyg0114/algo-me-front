f74b50adc3e6c1ba8ecbab1860f1526cc6f84f6a
lyg0114 <whdnseowkd@naver.com>
2024-02-28 10:30:00 +0100

    refactor : delete unnecessary code

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import {AuthProvider} from "./components/context/AuthContext";
 import {BrowserRouter as Router, Navigate, Route, Routes} from 'react-router-dom'
 import Login from "./components/Login";
 import PrivateRoute from "./components/util/PrivateRoute";
-import Layout from "./components/LayOut";
-import HomeDashBoard from "./components/user/home/dashboard/HomeDashBoard";
 
 function App() {
     return (
@@ -13,14 +11,10 @@ function App() {
                 <Routes>
                     <Route path='/' element={
                         <PrivateRoute>
-                            <Layout>
-                                <HomeDashBoard/>
-                            </Layout>
                         </PrivateRoute>
                     }/>
                     <Route path='/login' element={<Login/>}/>
                     <Route path="*" element={<Navigate to="/"/>}/>
-
                 </Routes>
             </Router>
         </AuthProvider>
