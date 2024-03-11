e29989b49177ab8b35a9b4e779575cb87ff16585
lyg0114 <whdnseowkd@naver.com>
2024-03-11 23:55:15 +0100

    style : update format

--- src/Constants.jsx
+++ src/Constants.jsx
@@ -1,13 +1,21 @@
 const prod = {
+
     url: {
+
         API_BASE_URL: 'https://lizcalendal.com/api',
+
     }
+
 }
 
 const dev = {
+
     url: {
+
         API_BASE_URL: 'http://localhost/api'
+
     }
+
 }
 
 export const config = process.env.NODE_ENV === 'development' ? dev : prod
