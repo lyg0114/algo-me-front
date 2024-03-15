a86a10dc0fd3beb603a34eae7007b96d75de2cc7
lyg0114 <whdnseowkd@naver.com>
2024-03-15 07:28:43 +0100

    feat : add chart1 image

--- src/components/user/landingpage/LandingPageContent.jsx
+++ src/components/user/landingpage/LandingPageContent.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import ChartImage from '../../assets/images/chart1.jpeg'
 
 function LandingPageContent() {
     return (
         <>
-            <Row style={{ height: '100%', background: 'rgb(15, 15, 15)' }} className="justify-content-between pl-3 pr-3 pt-4 pb-4 mb-2 fixed-top bg-black">
-                <Col style={{ height: '100%' }} className='' xs={6} lg={6}>
-                    CONTENT
+            <Row style={{height: '100vh', background: 'rgb(15, 15, 15)'}}
+                 className="justify-content-between align-items-center pl-3 pr-3 pt-4 pb-4 mb-2 fixed-top bg-black">
+                <Col style={{height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center'}} xs={6} lg={4}>
+                    <div style={{textAlign: 'center', fontSize:'xxx-large', color:'white'}}>
+                        <div> HOW DO </div>
+                        <div> YOU GET READY </div>
+                        <div> FOR REVIEW </div>
+                    </div>
                 </Col>
-                <Col style={{ height: '100%' }} className='' xs={6} lg={6}>
-                    CONTENT
+                <Col style={{height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center'}} xs={6} lg={8}>
+                    <div style={{textAlign: 'center'}}>
+                        <img src={ChartImage} alt="이미지 설명" style={{width: '100%', maxWidth: '1000px'}} />
+                    </div>
                 </Col>
             </Row>
         </>
