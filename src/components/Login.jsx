382febec56cc8b9c775be52ff9a3d9d0dee18c32
lyg0114 <whdnseowkd@naver.com>
2024-03-18 15:01:36 +0100

    fix : 로그인시 서버로부터의 메세지를 사용자에게 출력

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,87 +6,98 @@ import {handleLogError, parseJwt} from './util/Helpers';
 import {Button, Form} from 'react-bootstrap';
 
 function Login() {
-  const Auth = useAuth();
-  const isLoggedIn = Auth.userIsAuthenticated();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [isError, setIsError] = useState(false);
+    const Auth = useAuth();
+    const isLoggedIn = Auth.userIsAuthenticated();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [isValidated, setIsValidated] = useState(false);
+    const [message, setMessage] = useState('');
 
-  const handleInputChange = (e) => {
-    const {name, value} = e.target;
-    if (name === 'username') {
-      setUsername(value);
-    } else if (name === 'password') {
-      setPassword(value);
+
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+        if (name === 'username') {
+            setUsername(value);
+        } else if (name === 'password') {
+            setPassword(value);
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!(username && password)) {
+            setIsValidated(true);
+            return;
+        }
+
+        try {
+            const response = await backendApi.authenticate(username, password);
+            const accessToken = response.data.token;
+            const data = parseJwt(accessToken);
+            const authenticatedUser = {data, accessToken};
+            Auth.userLogin(authenticatedUser);
+            setUsername('');
+            setPassword('');
+            setIsValidated(false);
+        } catch (error) {
+            handleLogError(error);
+            setIsValidated(true);
+            setMessage(error.response.data.message);
+        }
+    };
+
+    if (isLoggedIn) {
+        return <Navigate to={'/main'}/>;
     }
-  };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (!(username && password)) {
-      setIsError(true);
-      return;
-    }
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-900">
+            <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+                <h2 className="text-3xl font-extrabold text-gray-900 mb-4">LOGIN</h2>
+                <Form onSubmit={handleSubmit}>
 
-    try {
-      const response = await backendApi.authenticate(username, password);
-      const accessToken = response.data.token;
-      const data = parseJwt(accessToken);
-      const authenticatedUser = {data, accessToken};
-      Auth.userLogin(authenticatedUser);
-      setUsername('');
-      setPassword('');
-      setIsError(false);
-    } catch (error) {
-      handleLogError(error);
-      setIsError(true);
-    }
-  };
+                    <Form.Group controlId="username" className='mb-2'>
+                        <Form.Label>ID</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={username}
+                            onChange={handleInputChange}
+                            placeholder="Enter your id"
+                            name="username"
+                            required
+                        />
+                    </Form.Group>
 
-  if (isLoggedIn) {
-    return <Navigate to={'/main'}/>;
-  }
+                    <Form.Group controlId="password" className='mb-4'>
+                        <Form.Label>PW</Form.Label>
+                        <Form.Control
+                            type="password"
+                            value={password}
+                            onChange={handleInputChange}
+                            placeholder="Enter your password"
+                            name="password"
+                            required
+                        />
+                    </Form.Group>
 
-  return (
-      <div className="min-h-screen flex items-center justify-center bg-gray-900">
-        <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
-          <h2 className="text-3xl font-extrabold text-gray-900 mb-4">LOGIN</h2>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="username">
-              <Form.Label>ID</Form.Label>
-              <Form.Control
-                  type="text"
-                  value={username}
-                  onChange={handleInputChange}
-                  placeholder="Enter your id"
-                  name="username"
-                  required
-              />
-            </Form.Group>
+                    <Button
+                        variant="primary"
+                        type="submit"
+                        className="w-full hover:bg-blue-600 transition duration-300"
+                    >
+                        Login
+                    </Button>
 
-            <Form.Group controlId="password">
-              <Form.Label>PW</Form.Label>
-              <Form.Control
-                  type="password"
-                  value={password}
-                  onChange={handleInputChange}
-                  placeholder="Enter your password"
-                  name="password"
-                  required
-              />
-            </Form.Group>
+                    {isValidated &&
+                        <div className='mt-4'>
+                            {message}
+                        </div>
+                    }
 
-            <Button
-                variant="primary"
-                type="submit"
-                className="w-full hover:bg-blue-600 transition duration-300"
-            >
-              Login
-            </Button>
-          </Form>
+                </Form>
+            </div>
         </div>
-      </div>
-  );
+    );
 }
 
 export default Login;
