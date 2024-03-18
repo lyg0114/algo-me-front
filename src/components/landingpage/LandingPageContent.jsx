56dec7595c97d87e51bf087863f5c2ddabef497a
lyg0114 <whdnseowkd@naver.com>
2024-03-18 16:14:57 +0100

    fix : 망각곡선 이미지명 변경

--- src/components/landingpage/LandingPageContent.jsx
+++ src/components/landingpage/LandingPageContent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import ChartImage from '../assets/images/chart1.jpeg'
+import ChartImage from '../assets/images/forgetting-curve.jpeg'
 import Button from "react-bootstrap/Button";
 import {useNavigate} from "react-router-dom";
 
