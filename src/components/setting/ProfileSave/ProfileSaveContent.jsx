024ad0463b16d77aae5da2e8a6a34e93e66fd69f
lyg0114 <whdnseowkd@naver.com>
2024-04-28 09:32:46 +0200

    feat, fix : 파일 업로드 부분을 axios 에서 fetch로 변경

--- src/components/setting/ProfileSave/ProfileSaveContent.jsx
+++ src/components/setting/ProfileSave/ProfileSaveContent.jsx
@@ -5,9 +5,6 @@ import {useNavigate, useParams} from "react-router-dom";
 import '@toast-ui/editor/dist/toastui-editor.css';
 import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
 import {useAuth} from "../../context/AuthContext";
-import {backendQuestionApi} from "../../util/api/BackendQuestionApi";
-import {handleLogError} from "../../util/Helpers";
-import {BackendProfileApi} from "../../util/api/BackendProfileApi";
 
 function ProfileSaveContent() {
     const Auth = useAuth();
@@ -20,78 +17,39 @@ function ProfileSaveContent() {
     const [content, setContent] = useState('');
     const navigate = useNavigate();
     const {id} = useParams();
-    const [isPageLoading, setIsPageLoading] = useState(true);
+    // const [isPageLoading, setIsPageLoading] = useState(true);
     const [isSaveLoading, setIsSaveLoading] = useState(false);
 
     const [selectedFile, setSelectedFile] = useState(null);
-
-    useEffect(() => {
-        fetchAndBindingQuestion();
-    }, []);
-
-    const fetchAndBindingQuestion = async () => {
-        try {
-            if (id) {
-                const response = await backendQuestionApi.getQuestion(user, id);
-                validate(response);
-                setQuestionType(response.data.questionType);
-                setFromSource(response.data.fromSource);
-                setTitle(response.data.title);
-                setUrl(response.data.url);
-                setContent(response.data.content);
-            }
-        } catch (error) {
-            handleLogError(error);
-        } finally {
-            setIsPageLoading(false);
-        }
-    };
-
-    const validate = (response) => {
-        if (response.data.content == null) {
-            response.data.content = '';
-        }
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        let response;
-        let content = editorRef.current.getInstance().getMarkdown();
-        let question = {title, url, fromSource, questionType, content};
-
-        try {
-            setIsSaveLoading(true);
-            if (id) {
-                response = await backendQuestionApi.updateQuestion(id, question, user);
-                clearInput();
-                navigate('/view-question/' + id);
-            } else {
-                response = await backendQuestionApi.addQuestion(question, user);
-                clearInput();
-                navigate('/view-question/' + response.data.id);
-            }
-        } catch (error) {
-            handleLogError(error)
-        } finally {
-            setIsSaveLoading(false);
-        }
-    };
-
-    const clearInput = () => {
-        setTitle("");
-        setUrl("");
-        setFromSource("");
-        setQuestionType("");
-    }
 
     const handleFileChange = (event) => {
         setSelectedFile(event.target.files[0]);
     }
 
-    const handleUpload = () => {
-        debugger
-        BackendProfileApi.uploadThumnail(selectedFile, user);
+    const handleUpload = async (event) => {
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append('file', selectedFile);
+        formData.append('name', 'test');
+        try {
+            const response = await fetch('http://localhost:3000/api/profile/upload', {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3MTQyOTI5NTksImlhdCI6MTcxNDI4OTM1OSwianRpIjoiNzE2YjY2NjQtMzdkYS00ZWIzLWJhNWEtMDk0MTNjOGUxMTk2IiwiaXNzIjoib3JkZXItYXBpIiwiYXVkIjoib3JkZXItYXBwIiwic3ViIjoidXNlckBleGFtcGxlLmNvbSIsInJvbGVzIjpbIlVTRVIiXSwibmFtZSI6Imt5bGUiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJ1c2VyQGV4YW1wbGUuY29tIiwiZW1haWwiOiJ1c2VyQGV4YW1wbGUuY29tIn0.WdWtLdrX0ncBQ_BLYfXEPCGQ7TOCz04-3VPBIk304SI',
+                }
+            });
+
+            if (response.ok) {
+                console.log('File uploaded successfully');
+            } else {
+                console.error('Failed to upload file:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Error uploading file:', error);
+        }
     };
+
 
     let colStyle = {
         paddingLeft: '0px',
@@ -100,58 +58,51 @@ function ProfileSaveContent() {
 
     return (
         <>
-            {isPageLoading ? ( // 로딩 중일 때
-                <div className="d-flex justify-content-center align-items-center" style={{minHeight: '80vh'}}>
-                    <Spinner animation="border" role="status">
-                        <span className="visually-hidden">Loading...</span>
-                    </Spinner>
-                </div>
-            ) : (
-                <Row className="g-4 mt-5 pl-5 pr-5" style={{height: '100vh'}}>
-                    <Col xs='auto' md={2} sm={2} lg={2} xl={2} xxl={2}></Col>
-                    <Col xs='auto' md={8} sm={8} lg={8} xl={8} xxl={8}>
+            <Row className="g-4 mt-5 pl-5 pr-5" style={{height: '100vh'}}>
+                <Col xs='auto' md={2} sm={2} lg={2} xl={2} xxl={2}></Col>
+                <Col xs='auto' md={8} sm={8} lg={8} xl={8} xxl={8}>
 
-                        <Form className='header' onSubmit={handleSubmit} style={{height: '50%', color: 'white', position: 'relative'}}>
-                            <Form.Group as={Row} className="mb-3">
-                                <Form.Label column sm={2}> 사용자명 </Form.Label>
-                                <Col sm={10} style={colStyle}>
-                                    <Form.Control
-                                        id="title"
-                                        name="title"
-                                        value={title}
-                                        type="text"
-                                        onChange={(e) => setTitle(e.target.value)}
-                                    />
-                                </Col>
-                            </Form.Group>
+                    {/*<Form className='header' onSubmit={handleSubmit} style={{height: '50%', color: 'white', position: 'relative'}}>*/}
+                    {/*    <Form.Group as={Row} className="mb-3">*/}
+                    {/*        <Form.Label column sm={2}> 사용자명 </Form.Label>*/}
+                    {/*        <Col sm={10} style={colStyle}>*/}
+                    {/*            <Form.Control*/}
+                    {/*                id="title"*/}
+                    {/*                name="title"*/}
+                    {/*                value={title}*/}
+                    {/*                type="text"*/}
+                    {/*                onChange={(e) => setTitle(e.target.value)}*/}
+                    {/*            />*/}
+                    {/*        </Col>*/}
+                    {/*    </Form.Group>*/}
 
-                            <Button style={{
-                                position: 'absolute',
-                                right: 0,
-                                marginTop: '7px',
-                                marginLeft: '7px'
-                            }} type="submit" disabled={isSaveLoading}>
-                                {isSaveLoading ? (
-                                    <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true"/>
-                                ) : (
-                                    '저장'
-                                )}
-                            </Button>
-                        </Form>
+                    {/*    <Button style={{*/}
+                    {/*        position: 'absolute',*/}
+                    {/*        right: 0,*/}
+                    {/*        marginTop: '7px',*/}
+                    {/*        marginLeft: '7px'*/}
+                    {/*    }} type="submit" disabled={isSaveLoading}>*/}
+                    {/*        {isSaveLoading ? (*/}
+                    {/*            <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true"/>*/}
+                    {/*        ) : (*/}
+                    {/*            '저장'*/}
+                    {/*        )}*/}
+                    {/*    </Button>*/}
+                    {/*</Form>*/}
 
-                        <div>
-                            <form>
-                                <input type="file" onChange={handleFileChange} />
-                                <button onClick={handleUpload}>Upload</button>
-                            </form>
-                        </div>
+                    <div>
+                        <form>
+                            <input type="file" onChange={handleFileChange} />
+                            <button onClick={handleUpload}>Upload</button>
+                        </form>
+                    </div>
 
 
-                    </Col>
-                    <Col sm={2} md={2} lg={2} xl={2} xxl={2}></Col>
-                </Row>
-            )}
+                </Col>
+                <Col sm={2} md={2} lg={2} xl={2} xxl={2}></Col>
+            </Row>
         </>
+
     );
 }
 
--- src/components/util/api/BackendProfileApi.jsx
+++ src/components/util/api/BackendProfileApi.jsx
@@ -1,27 +1,44 @@
 import instance from "./BackendInstance";
 import {BackendAuthApi as backendAuthApi} from "./BackendAuthApi";
+import axios from "axios";
 
+const BASE_URL = "http://localhost:3000/api/profile"
 export const BackendProfileApi = {
     getProfile,
     updateProfile,
     uploadThumnail,
 }
 
-function uploadThumnail(selectedFile, user) {
-    const formData = new FormData();
-    formData.append('file', selectedFile);
+function uploadThumnail(formData) {
     debugger
-    instance.post(`/profile/upload`, formData, {
-        headers: {
-            'Content-type': 'multipart/form-data',
-            'Authorization': backendAuthApi.bearerAuth(user)
+    return axios.post(
+        BASE_URL + '/upload',
+        formData,
+        {
+            headers: {
+                'Content-type': 'multipart/form-data',
+                'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3MTQyOTIyNTcsImlhdCI6MTcxNDI4ODY1NywianRpIjoiMzJiZjIwMDYtYTBhNi00MTJhLTk3M2YtZThlNWE1NGU2YWYzIiwiaXNzIjoib3JkZXItYXBpIiwiYXVkIjoib3JkZXItYXBwIiwic3ViIjoidXNlckBleGFtcGxlLmNvbSIsInJvbGVzIjpbIlVTRVIiXSwibmFtZSI6Imt5bGUiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJ1c2VyQGV4YW1wbGUuY29tIiwiZW1haWwiOiJ1c2VyQGV4YW1wbGUuY29tIn0.Rj5Qck0vP55GaxrS5jsYfRhEmWqotYfkQGsuljtlEqQ'
+            }
         }
-    }).then(response => {
-        console.log('File uploaded successfully:', response.data); // 파일 업로드 후 필요한 작업 수행
-    }).catch(error => {
-        console.error('Error uploading file:', error);
-    });
+    );
 }
+
+
+// function uploadThumnail(formData, user) {
+//     debugger
+//     instance.post(`/profile/upload`,
+//         formData,
+//         {
+//             headers: {
+//                 'Content-type': 'multipart/form-data',
+//                 'Authorization': backendAuthApi.bearerAuth(user)
+//             }
+//         }).then(response => {
+//         console.log('File uploaded successfully:', response.data); // 파일 업로드 후 필요한 작업 수행
+//     }).catch(error => {
+//         console.error('Error uploading file:', error);
+//     });
+// }
 
 function getProfile(user, id) {
     const url = `/questions/${id}`;
